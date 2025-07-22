c3063eebe4d439245a0afc21be43081a86f969d1
Robin Markowitz <robin.mwitz@gmail.com>
2025-07-21 23:36:14 -0700

    Code cleanup

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import QuickCart from './components/quick-cart.jsx';
+import QuickCart from './components/quick-cart.js';
 
 ReactDOM.render(<QuickCart item={parseInt(window.location.href.split('/').pop()) || 0} />, document.getElementById('quick-cart-app'));
--- public/reducers/mini-cart-reducer.js
+++ public/reducers/mini-cart-reducer.js
@@ -18,4 +18,4 @@ export default function miniCartReducer(state, action) {
 		default:
 			break;
 	}
-}
+};
