ac25d1ca6a24db12572dad1e8c1b1dcd1d6d5a34
Robin Markowitz <robin.mwitz@gmail.com>
2025-07-21 21:28:26 -0700

    Refactor mini-cart.js
    React 19 is very different from React 16. The docs encourage you to
    refactor components using classes to functional components. The main
    differences in this module are the change from using React 16 state
    to using the useReducer hook and using lifecycle methods to using
    the useEffect hook. I enjoyed the precise language provided by life-
    cycle methods but dependencies do make things a little more intuitive
    at the cost of being a little magical. Dispatching state, however,
    is definitely how these apps should be written in the first place. It
    allows you to leverage state management tools like Redux with very
    little refactoring and without changing the design of components. The
    useReducer hook is a welcome change.

--- public/components/mini-cart.js
+++ public/components/mini-cart.js
@@ -1,85 +1,83 @@
-import React from 'react';
+import { useEffect, useReducer } from 'react';
+import miniCartReducer from '../reducers/mini-cart-reducer.js';
 
-class MiniCart extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      viewClass: this.props.cartSize > 0 ? 'minicart-view hidden' : 'minicart-view empty hidden',
-      timeoutID: '',
-    };
+export default function MiniCart (props) {
+  const [state, dispatch] = useReducer(miniCartReducer, {
+    viewClass: props.cartSize > 0 ? 'minicart-view hidden' : 'minicart-view empty hidden',
+    timeoutID: '',
+  });
 
-    this.generateMiniCart = this.generateMiniCart.bind(this);
-    this.showCart = this.showCart.bind(this);
-    this.hideCart = this.hideCart.bind(this);
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.cartSize < this.props.cartSize) {
-      this.setState({
-        viewClass: 'minicart-view',
-      }, this.hideCart.bind(null, false));
-    }
-  }
-
-  showCart() {
-    if (this.state.timeoutID) {
-      clearTimeout(this.state.timeoutID);
-    }
-
-    this.setState({
-      viewClass: this.state.viewClass.replace(' hidden', ''),
+  // Show cart when an item is added and start a timeout to hide it
+  useEffect(() => {
+    dispatch({
+      type: 'item_added',
+      viewClass: 'minicart-view',
     });
-  }
+  }, [props.cartSize]);
+  useEffect(() => {
+    hideCart(false);
+  }, [state.viewClass]);
 
-  hideCart(isImmediate) {
-    if (this.state.timeoutID) {
-      clearTimeout(this.state.timeoutID);
+  const stopHiding = () {
+    if (state.timeoutID) {
+      clearTimeout(state.timeoutID);
+    }
+  };
+
+  const hideCart = isImmediate => {
+    // Reset the timeout on consecutive calls
+    if (state.timeoutID) {
+      clearTimeout(state.timeoutID);
     }
 
     if (isImmediate) {
-      this.setState({
-        viewClass: `${this.state.viewClass} hidden`,
+      dispatch({
+        type: 'hide_immediate',
+        viewClass: `${viewClass} hidden`,
         timeoutID: '',
       });
     } else {
-      const newTimeoutID = setTimeout(() => this.setState({
-        viewClass: `${this.state.viewClass} hidden`,
+      const newTimeoutID = setTimeout(() => dispatch({
+        type: 'hide_immediate',
+        viewClass: `${viewClass} hidden`,
+        timeoutID: '',
       }), 5000);
 
-      this.setState({
+      dispatch({
+        type: 'hide_timeout',
         timeoutID: newTimeoutID,
       });
     }
-  }
+  };
 
-  calculateTotal() {
+  const calculateTotal = () => {
     let sum = 0;
-    for (let key in this.props.cart) {
-      sum += this.props.cart[key].price * this.props.cart[key].quantity;
+    for (let key in props.cart) {
+      sum += props.cart[key].price * props.cart[key].quantity;
     }
     return sum;
-  }
+  };
 
-  generateMiniCart(cartSize) {
+  const generateMiniCart = cartSize => {
     if (cartSize > 0) {
       const newCart = [];
 
-      for (let i = 0; i < this.props.cartOrder.length; i += 1) {
-        const currItem = this.props.cartOrder[i];
+      for (let i = 0; i < props.cartOrder.length; i += 1) {
+        const currItem = props.cartOrder[i];
         newCart.push(
           <div className="cart-item" key={`cartItem${i}`}>
-            <img className="cart-item-img" src={this.props.cart[currItem].imgUrl}/>
+            <img className="cart-item-img" src={props.cart[currItem].imgUrl}/>
             <div className="cart-item-info">
               <span
                 className="cart-item-info name"
-                onClick={this.props.getNewPage.bind(null, this.props.cart[currItem].id)}>
-                {this.props.cart[currItem].name}
+                onClick={props.getNewPage.bind(null, props.cart[currItem].id)}>
+                {props.cart[currItem].name}
               </span>
-              <span className="cart-item-info qty">Quantity: {this.props.cart[currItem].quantity}</span>
-              <span className="cart-item-info color">Color: {this.props.cart[currItem].color}</span>
-              <span className="cart-item-info size">Size: {this.props.cart[currItem].size}</span>
+              <span className="cart-item-info qty">Quantity: {props.cart[currItem].quantity}</span>
+              <span className="cart-item-info color">Color: {props.cart[currItem].color}</span>
+              <span className="cart-item-info size">Size: {props.cart[currItem].size}</span>
             </div>
-            <span className="cart-item-price">${this.props.cart[currItem].price / 100}</span>
+            <span className="cart-item-price">${props.cart[currItem].price / 100}</span>
           </div>
         );
       }
@@ -89,36 +87,32 @@ class MiniCart extends React.Component {
           {newCart}
         </div>,
         <div className="minicart-total" key="cartTotal">
-          <span className="total-item-count">TOTAL ({this.props.cartSize} ITEMS)</span>
-          <span className="total-subtotal">${this.calculateTotal() / 100}</span>
+          <span className="total-item-count">TOTAL ({props.cartSize} ITEMS)</span>
+          <span className="total-subtotal">${calculateTotal() / 100}</span>
         </div>,
         <div className="minicart-cart-controls" key="cartControls">
           <button className="minicart-view-cart-btn">VIEW BAG</button>
           <button className="minicart-checkout-btn">CHECKOUT</button>
         </div>,
-        <span className="minicart-close-btn" key="closeBag" onClick={this.hideCart.bind(null, true)}>CLOSE BAG</span>,
+        <span className="minicart-close-btn" key="closeBag" onClick={hideCart.bind(null, true)}>CLOSE BAG</span>,
       ];
     } else {
       return 'YOUR BAG IS EMPTY';
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className="minicart">
-        <div className="minicart-icon"
-            onMouseEnter={this.showCart}
-            onMouseLeave={this.hideCart.bind(null, false)}>
-          <span>{this.props.cartSize}</span>
-        </div>
-        <div className={this.state.viewClass}
+  return (
+    <div className="minicart">
+      <div className="minicart-icon"
           onMouseEnter={this.showCart}
           onMouseLeave={this.hideCart.bind(null, false)}>
-          {this.generateMiniCart(this.props.cartSize)}
-        </div>
+        <span>{this.props.cartSize}</span>
       </div>
-    );
-  }
-};
-
-export default MiniCart;
+      <div className={this.state.viewClass}
+        onMouseEnter={this.showCart}
+        onMouseLeave={this.hideCart.bind(null, false)}>
+        {this.generateMiniCart(this.props.cartSize)}
+      </div>
+    </div>
+  );
+}
---
+++ public/reducers/mini-cart-reducer.js
@@ -0,0 +1,21 @@
+export default function miniCartReducer(state, action) {
+	switch (action.type) {
+		case 'item_added':
+			return {
+				viewClass: action.viewClass,
+				timeoutID: state.timeoutID,
+			};
+		case 'hide_immediate':
+			return {
+				viewClass: action.viewClass,
+				timeoutID: action.timeoutID,
+			};
+		case 'hide_delayed':
+			return {
+				viewClass: state.viewClass,
+				timeoutID: action.timeoutID,
+			};
+		default:
+			break;
+	}
+}
