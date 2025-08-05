ee767699889509f160d01b284a9352353976c9a8
Robin Markowitz <robin.mwitz@gmail.com>
2025-08-05 14:49:25 -0700

    Migrate eslint config to new version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,43 @@
-import js from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+export default defineConfig([globalIgnores(["client/bundle.js", "**/node_modules", "**/coverage"]), {
+    extends: compat.extends("airbnb"),
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+            ...globals.jquery,
+        },
+
+        ecmaVersion: 6,
+        sourceType: "module",
+
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
+
+    rules: {
+        "no-console": "off",
+        "max-len": "off",
+        "no-undef": "off",
+        "no-unused-vars": "off",
+        "no-loop-func": "off",
+    },
+}]);
